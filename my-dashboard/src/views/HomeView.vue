ba498f44401ffadc6aa8524acc1777f2bcc3d700
Eli Galayda <eli.galayda@slalom.com>
2026-04-02 12:31:11 -0500

    fix: remove unused TheWelcome component import from HomeView.vue

--- my-dashboard/src/views/HomeView.vue
+++ my-dashboard/src/views/HomeView.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
-<script setup lang="ts">
 import { Bar, Line, Doughnut } from 'vue-chartjs'
 import {
   Chart as ChartJS,
